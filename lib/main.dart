77cf526c823f668f9693c8104b542d624e4db367
pduebel <pnduebel@gmail.com>
2021-10-26 08:45:17 +0100

    add wordpair list tiles and page for saved wordpairs

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.teal),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('WordPair Generator'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
